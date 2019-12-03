96d03e2fc7715c11c46de06379de150e6350458f
guolei <174000902@qq.com>
2019-12-03 14:29:35 +0800

    guolei update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(name="guolei_py3_components",
-      version="1.0.2",
+      version="1.0.3",
       description="a python3 components by guolei",
       long_description=long_description,
       long_description_content_type="text/markdown",
