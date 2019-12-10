b03f0341f072cd0baa190ce65ee4c1c6d09fa89c
guolei <174000902@qq.com>
2019-12-10 16:30:49 +0800

    guolei update

--- tests.py
+++ tests.py
@@ -15,32 +15,7 @@ class UtilTest(unittest.TestCase):
         print("util test up by {start_time_str}".format(start_time_str=util.get_time_str()))
 
     def test_run(self):
-        logging_logger_obj = logger.get_logging_logger_obj(is_save_file=True)
-        mysql_config = {
-            "host": "121.42.166.250",
-            "port": 3306,
-            "user": "luis",
-            "passwd": "123456",
-            "db": "polyrich",
-            "charset": "utf8",
-            "cursorclass": pymysql.cursors.DictCursor
-        }
-        pymysql_conn_obj = database.get_pymysql_connection(**mysql_config)
 
-        @logger.call_logging_logger_log(logging_logger_obj, "info", "接收返回值")
-        @database.call_pymysql_find_first(pymysql_conn_obj)
-        @logger.call_logging_logger_log(logging_logger_obj, "info", "接收返回值1")
-        def a(query_str="", args={}):
-            @util.call_str_to_upper
-            def to_upper():
-                return query_str
-
-            print(to_upper())
-            query = query_str
-            args = None
-            return query, args
-
-        print(a("select * from guolei_temp_201904191327;"))
         pass
 
 
